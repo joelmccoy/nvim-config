d260c4c2d526b04d7f7298ffa4b8f7d8c8221440
Joel McCoy <nbajoel@gmail.com>
2024-03-05 16:40:47 -0600

    setup format on save

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -67,6 +67,14 @@ return {
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+
+			-- Setup format on save
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				buffer = buffer,
+				callback = function()
+					vim.lsp.buf.format({ async = false })
+				end,
+			})
 		end,
 	},
 }
