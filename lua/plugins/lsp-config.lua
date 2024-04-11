e979addc321091e06427b8c85c1811704154de27
Joel McCoy <nbajoel@gmail.com>
2024-04-11 09:16:39 -0500

    added command to view full diagnostics

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -36,6 +36,7 @@ return {
 				-- Mappings.
 				-- See `:help vim.lsp.*` for documentation on any of the below functions
 				local bufopts = { noremap = true, silent = true, buffer = bufnr }
+				vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, bufopts)
 				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
 				vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
 				vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
