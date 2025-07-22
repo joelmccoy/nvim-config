360706b594367508e94bfe49d72a4a0002fc0229
Joel McCoy <joel@defenseunicorns.com>
2025-07-22 09:58:33 -0500

    chore: add error messages to null-ls

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -9,6 +9,15 @@ return {
         null_ls.builtins.formatting.prettierd,
       },
     })
+
+    vim.diagnostic.config({
+      virtual_text = true, -- show message inline
+      signs = true,        -- signs in gutter
+      underline = true,    -- underline in code
+      update_in_insert = false,
+      severity_sort = true,
+    })
+
     vim.keymap.set("n", "<space>f", function()
       vim.lsp.buf.format({ async = true })
     end, {})
