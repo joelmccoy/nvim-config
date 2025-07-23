ac253a4fa56c9f40ab66af74f09e1141a8e6eae4
Joel McCoy <joel@defenseunicorns.com>
2025-07-23 13:37:06 -0500

    fix: ts lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,155 +1,155 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "gopls",
-          "pyright",
-          -- "templ",
-          -- "tailwindcss",
-          -- "jsonls",
-          "ts_ls",
-          "terraformls",
-          "tflint",
-          "typos_lsp",
-          -- "yamlls",
-          "helm_ls"
-        },
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local lspconfig = require("lspconfig")
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"gopls",
+					"pyright",
+					-- "templ",
+					-- "tailwindcss",
+					-- "jsonls",
+					"ts_ls",
+					"terraformls",
+					"tflint",
+					"typos_lsp",
+					-- "yamlls",
+					"helm_ls",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
 
-      local on_attach = function(client, bufnr)
-        -- Enable completion triggered by <c-x><c-o>
-        vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+			local on_attach = function(client, bufnr)
+				-- Enable completion triggered by <c-x><c-o>
+				vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-        -- Mappings.
-        -- See `:help vim.lsp.*` for documentation on any of the below functions
-        local bufopts = { noremap = true, silent = true, buffer = bufnr }
-        vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, bufopts)
-        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-        vim.keymap.set("n", "<c-K>", vim.lsp.buf.hover, bufopts)
-        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
-        vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
-        vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
-        vim.keymap.set("n", "<space>wl", function()
-          print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        end, bufopts)
-        vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
-        vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
-        vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
-        vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
-        vim.keymap.set("n", "<space>f", function()
-          vim.lsp.buf.format({ async = true })
-        end, bufopts)
-      end
+				-- Mappings.
+				-- See `:help vim.lsp.*` for documentation on any of the below functions
+				local bufopts = { noremap = true, silent = true, buffer = bufnr }
+				vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, bufopts)
+				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+				vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+				vim.keymap.set("n", "<c-K>", vim.lsp.buf.hover, bufopts)
+				vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+				vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
+				vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
+				vim.keymap.set("n", "<space>wl", function()
+					print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+				end, bufopts)
+				vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
+				vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
+				vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
+				vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+				vim.keymap.set("n", "<space>f", function()
+					vim.lsp.buf.format({ async = true })
+				end, bufopts)
+			end
 
-      lspconfig.lua_ls.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-      })
-      lspconfig.gopls.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-      })
-      lspconfig.pyright.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-      })
-      lspconfig.templ.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-      })
-      lspconfig.tailwindcss.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-      })
-      -- lspconfig.jsonls.setup({
-      --   on_attach = on_attach,
-      --   capabilities = capabilities,
-      -- })
-      lspconfig.ts_ls.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-      })
-      lspconfig.terraformls.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-      })
-      lspconfig.tflint.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-      })
-      lspconfig.typos_lsp.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-      })
-      -- lspconfig.yamlls.setup({
-      --   on_attach = on_attach,
-      --   capabilities = capabilities,
-      --   schemaStore = {
-      --     enable = true,
-      --     url = "https://www.schemastore.org/api/json/catalog.json",
-      --   },
-      --   schemas = {
-      --     ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
-      --     ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-      --     ["https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/master/service-schema.json"] =
-      --     "azure-pipelines.{yml,yaml}",
-      --     ["https://raw.githubusercontent.com/ansible/ansible-lint/main/src/ansiblelint/schemas/ansible.json#/$defs/tasks"] =
-      --     "roles/tasks/*.{yml,yaml}",
-      --     ["https://raw.githubusercontent.com/ansible/ansible-lint/main/src/ansiblelint/schemas/ansible.json#/$defs/playbook"] =
-      --     "*play*.{yml,yaml}",
-      --     ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
-      --     ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
-      --     ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
-      --     ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
-      --     ["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] =
-      --     "*gitlab-ci*.{yml,yaml}",
-      --     ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] =
-      --     "*api*.{yml,yaml}",
-      --     ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] =
-      --     "*docker-compose*.{yml,yaml}",
-      --     ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] =
-      --     "*flow*.{yml,yaml}",
-      --     ["https://raw.githubusercontent.com/defenseunicorns/uds-cli/v0.12.0/uds.schema.json"] = "uds-bundle.yaml",
-      --     ["https://raw.githubusercontent.com/defenseunicorns/uds-cli/v0.12.0/tasks.schema.json"] = "tasks.yaml",
-      --     ["https://raw.githubusercontent.com/defenseunicorns/uds-cli/v0.12.0/zarf.schema.json"] = "zarf.yaml",
-      --   },
-      --   format = { enabled = true },
-      --   validate = true,
-      --   completion = true,
-      --   hover = true,
-      -- })
-      lspconfig.helm_ls.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-      })
+			lspconfig.lua_ls.setup({
+				on_attach = on_attach,
+				capabilities = capabilities,
+			})
+			lspconfig.gopls.setup({
+				on_attach = on_attach,
+				capabilities = capabilities,
+			})
+			lspconfig.pyright.setup({
+				on_attach = on_attach,
+				capabilities = capabilities,
+			})
+			lspconfig.templ.setup({
+				on_attach = on_attach,
+				capabilities = capabilities,
+			})
+			lspconfig.tailwindcss.setup({
+				on_attach = on_attach,
+				capabilities = capabilities,
+			})
+			-- lspconfig.jsonls.setup({
+			--   on_attach = on_attach,
+			--   capabilities = capabilities,
+			-- })
+			lspconfig.ts_ls.setup({
+				on_attach = on_attach,
+				capabilities = capabilities,
+			})
+			lspconfig.terraformls.setup({
+				on_attach = on_attach,
+				capabilities = capabilities,
+			})
+			lspconfig.tflint.setup({
+				on_attach = on_attach,
+				capabilities = capabilities,
+			})
+			lspconfig.typos_lsp.setup({
+				on_attach = on_attach,
+				capabilities = capabilities,
+			})
+			-- lspconfig.yamlls.setup({
+			--   on_attach = on_attach,
+			--   capabilities = capabilities,
+			--   schemaStore = {
+			--     enable = true,
+			--     url = "https://www.schemastore.org/api/json/catalog.json",
+			--   },
+			--   schemas = {
+			--     ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+			--     ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+			--     ["https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/master/service-schema.json"] =
+			--     "azure-pipelines.{yml,yaml}",
+			--     ["https://raw.githubusercontent.com/ansible/ansible-lint/main/src/ansiblelint/schemas/ansible.json#/$defs/tasks"] =
+			--     "roles/tasks/*.{yml,yaml}",
+			--     ["https://raw.githubusercontent.com/ansible/ansible-lint/main/src/ansiblelint/schemas/ansible.json#/$defs/playbook"] =
+			--     "*play*.{yml,yaml}",
+			--     ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+			--     ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+			--     ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+			--     ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
+			--     ["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] =
+			--     "*gitlab-ci*.{yml,yaml}",
+			--     ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] =
+			--     "*api*.{yml,yaml}",
+			--     ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] =
+			--     "*docker-compose*.{yml,yaml}",
+			--     ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] =
+			--     "*flow*.{yml,yaml}",
+			--     ["https://raw.githubusercontent.com/defenseunicorns/uds-cli/v0.12.0/uds.schema.json"] = "uds-bundle.yaml",
+			--     ["https://raw.githubusercontent.com/defenseunicorns/uds-cli/v0.12.0/tasks.schema.json"] = "tasks.yaml",
+			--     ["https://raw.githubusercontent.com/defenseunicorns/uds-cli/v0.12.0/zarf.schema.json"] = "zarf.yaml",
+			--   },
+			--   format = { enabled = true },
+			--   validate = true,
+			--   completion = true,
+			--   hover = true,
+			-- })
+			lspconfig.helm_ls.setup({
+				on_attach = on_attach,
+				capabilities = capabilities,
+			})
 
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
 
-      -- Setup format on save
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        buffer = buffer,
-        callback = function()
-          vim.lsp.buf.format({ async = false })
-        end,
-      })
-    end,
-  },
+			-- Setup format on save
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				buffer = buffer,
+				callback = function()
+					vim.lsp.buf.format({ async = false })
+				end,
+			})
+		end,
+	},
 }
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,25 +1,25 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        -- Lua
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettierd,
-      },
-    })
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				-- Lua
+				null_ls.builtins.formatting.stylua,
+				-- null_ls.builtins.formatting.prettierd,
+			},
+		})
 
-    vim.diagnostic.config({
-      virtual_text = true, -- show message inline
-      signs = true,        -- signs in gutter
-      underline = true,    -- underline in code
-      update_in_insert = false,
-      severity_sort = true,
-    })
+		vim.diagnostic.config({
+			virtual_text = true, -- show message inline
+			signs = true, -- signs in gutter
+			underline = true, -- underline in code
+			update_in_insert = false,
+			severity_sort = true,
+		})
 
-    vim.keymap.set("n", "<space>f", function()
-      vim.lsp.buf.format({ async = true })
-    end, {})
-  end,
+		vim.keymap.set("n", "<space>f", function()
+			vim.lsp.buf.format({ async = true })
+		end, {})
+	end,
 }
